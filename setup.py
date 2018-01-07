df485c047b9797eae1c262c44eff59728637275c
jordan-melendez <jmelendez1992@gmail.com>
2018-01-07 11:56:14 -0500

    Added a setup file for installation with pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='rrplot',
+    # packages=['rrplot'],
+    py_modules=['rrplot'],
+    version='0.1',
+    description='A python plotting library for reproducible research.',
+    author='Jordan Melendez',
+    author_email='jmelendez1992@gmail.com',
+    license='MIT',
+    url='https://github.com/jordan-melendez/rrplot',
+    # download_url='https://github.com/jordan-melendez/rrplot/archive/v0.1.tar.gz',
+    keywords='reproducible research plot nuclear physics physical review',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'
+        ]
+)
