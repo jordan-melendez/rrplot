2a56d838e59810d1bda719c20f2b2fd900070955
jordan-melendez <jmelendez1992@gmail.com>
2018-01-07 11:57:49 -0500

    Added skeleton of utility functions

---
+++ rrplot/utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+def findTeXFigureNames():
+    pass
+
+
+def placeFigures(currentDir, targetDir, names):
+    pass
+
+
+def labelMultipart(fig, label, pos):
+    pass
